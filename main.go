3fa1ea0170b6995ad6f539fc315af5198b762615
zhuhuanhuan <zhuhuamhuan@mistong.com>
2018-09-11 21:00:17 +0800

    modify:readme file

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"os"
+	"io"
+	"time"
+	"log"
 )
 
 var DB = make(map[string]string)
@@ -58,9 +62,38 @@ func setupRouter() *gin.Engine {
 	return r
 }
 
+func initLogger() {
+	logFile, _ := os.Create("./loggers/gin.log")
+	gin.DefaultWriter = io.MultiWriter(logFile)
+}
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		// Set example variable
+		c.Set("example", "12345")
+
+		// before request
+
+		c.Next()
+
+		// after request
+		latency := time.Since(t)
+		log.Print(latency)
+
+		// access the status we are sending
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
+
 func main() {
 
 	r := setupRouter()
+
+	initLogger()
+
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
